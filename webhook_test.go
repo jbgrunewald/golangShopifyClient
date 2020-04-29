8c5ea773688988c424144b21b29bc685a9b5d914
James Grunewald <jbgrunewald@gmail.com>
2020-04-28 21:32:58 -0700

    Add Webhook List Unit Test

--- webhook_test.go
+++ webhook_test.go
@@ -24,14 +24,63 @@ var createResponse = WebhookWrapper{
 	},
 }
 
-func TestWebhookCreate(t *testing.T) {
+var listResponse = WebhooksWrapper{
+	Webhooks: make([]Webhook, 1),
+}
+
+func TestWebhookList(t *testing.T) {
+	listResponse.Webhooks = append(listResponse.Webhooks, *createResponse.Webhook)
 	server := httptest.NewTLSServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.Path != "/admin/api/2019-07/webhooks.json" {
-			rw.Write([]byte("something went wrong"))
+		if req.URL.Path == "/admin/api/2019-07/webhooks.json" && req.Method == "GET" && req.URL.RawQuery == "limit=65&since_id=0" {
+			response, _ := json.Marshal(listResponse)
+			_, _ = rw.Write(response)
+			return
 		}
 
-		response, _ := json.Marshal(createResponse)
-		_, _ = rw.Write(response)
+		rw.Write([]byte("something went wrong"))
+	}))
+	defer server.Close()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	client := RestAdminClient{
+		Http:    server.Client(),
+		Logger:  logger,
+		Version: VERSION_2019_07,
+	}
+
+	serverUrl, _ := url.Parse(server.URL)
+	requestContext := ShopifyContext{
+		AccessToken: "thisisatoken",
+		ShopName:    serverUrl.Host,
+		Ctx:         context.Background(),
+	}
+
+	options := WebHookRequestOptions{
+		Limit: 65,
+	}
+	result, err := client.WebhookList(requestContext, options)
+	if err != nil {
+		t.Error(err)
+	}
+	resultStringed, err := json.Marshal(result)
+	if err != nil {
+		t.Error(err)
+	}
+	expected, _ := json.Marshal(listResponse.Webhooks)
+	if bytes.Compare(resultStringed, expected) != 0 {
+		t.Error()
+	}
+}
+
+func TestWebhookCreate(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/admin/api/2019-07/webhooks.json" && req.Method == "POST" {
+			response, _ := json.Marshal(createResponse)
+			_, _ = rw.Write(response)
+			return
+		}
+
+		rw.Write([]byte("something went wrong"))
 	}))
 	defer server.Close()
 
